c172d25f09efa98c31fa8690db0f23921b4d8bb9
janpaet-ghub <janpaet@googlemail.com>
2025-08-06 20:24:57 +0200

    Update ScmCoreJPAIntTestConfig.java

--- ScmCoreJPAIntTestConfig.java
+++ ScmCoreJPAIntTestConfig.java
@@ -1,6 +1,8 @@
 package de.fms.scm.config;
 
-import de.fms.scm.utils.LogUtil;
+import jakarta.persistence.EntityManagerFactory;
+
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +17,15 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Properties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
 @Profile(value = "testCore")
 @PropertySource("classpath:/core.configuration.properties")
 public class ScmCoreJPAIntTestConfig {
-
-    private static final LogUtil LOG = new LogUtil(ScmCoreJPAIntTestConfig.class);
-
-    private static final String PACKAGES_TO_SCAN = "de.fms.scm.entities";
 
     @Value("${db.url}")
     private String DB_URL;
@@ -60,45 +60,37 @@ public class ScmCoreJPAIntTestConfig {
         dataSource.setUrl(DB_URL);
         dataSource.setUser(DB_USERNAME);
         dataSource.setPassword(DB_PASSWORD);
-
         return dataSource;
     }
-
-    @Bean(name = "entityManagerFactoryBean")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(final DataSource dataSource) {
-        LOG.info("... create bean entityManagerFactoryBean");
-        final LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setJpaVendorAdapter(getJpaVendorAdapter());
-        emf.setDataSource(dataSource);
-        emf.setJpaProperties(getJpaProperties());
-        emf.setPackagesToScan(PACKAGES_TO_SCAN);
-        return emf;
-    }
+    
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            final DataSource dataSource,
+            final EntityManagerFactoryBuilder builder
+    ) {
+        return builder
+                .dataSource(dataSource)
+                .packages("de.fms.scm.entities")
+                .properties(getJpaProperties())
+                .build();
+    }    
 
     @Bean(name = "getJpaVendorAdapter")
     public JpaVendorAdapter getJpaVendorAdapter() {
         return new HibernateJpaVendorAdapter();
-    }
+    }   
 
-    @Bean(name = "emTxManager")
-    public PlatformTransactionManager emTxManager() {
-        LOG.info("... create bean emTxManager");
-        final JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactoryBean(dataSource()).getObject());
-        return transactionManager;
-    }
+    @Bean
+    PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }    
 
-    /**
-     * Configuration properties related to Hibernate
-     * 
-     * @return The Hibernate properties
-     */
-    final Properties getJpaProperties() {
-        final Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", HIBERNATE_DIALECT);
-        properties.setProperty("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DLL_AUTO);
-        properties.setProperty("hibernate.show_sql", HIBERNATE_SHOW_SQL);
-        properties.setProperty("hibernate.format_sql", HIBERNATE_FORMAT_SQL);
+    private Map<String, String> getJpaProperties() {
+    	final Map<String, String> properties = new HashMap<>();
+    	properties.put("hibernate.dialect", HIBERNATE_DIALECT);
+    	properties.put("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DLL_AUTO);
+    	properties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
+    	properties.put("hibernate.format_sql", HIBERNATE_FORMAT_SQL);
         return properties;
     }
 }
